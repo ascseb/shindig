efb41553d7eae6f160229aa8620ada7e0bc5e542
Woody Gilk <woody.gilk@kohanaphp.com>
2009-11-18 16:55:29 -0600

    Updated Controller_Media, massive simplification to sending files

--- classes/controller/shindig/media.php
+++ classes/controller/shindig/media.php
@@ -1,67 +1,34 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-class Controller_Shindig_Media extends Controller
-{
-	protected $_images = array('jpg','png','gif');
-	
+class Controller_Shindig_Media extends Controller {
+
 	public function action_get()
 	{
-		$string = $this->request->param('file');
-		$ext = $this->get_extension($string);
-		$this->display_file($this->strip_extension($string, $ext),$ext);
-	}
-	
-	/**
-	 * protected method for stripping extension off a string
-	 *
-	 * @param string $string	string to be processed
-	 * @param string $ext		extension to strip off string
-	 * @return string		 	returns string with extension stripped
-	 */
-	protected function strip_extension($string, $ext)
-	{
-		$ext_pos = 0 - strlen( '.'.$ext );
-		$has_ext = ( substr($string,$ext_pos) == '.'.$ext ) ? TRUE : FALSE ;
-		return ( $has_ext ) ? substr($string,0,$ext_pos) : $string;
-	}
-	
-	/**
-	 * Returns the extension of the file
-	 *
-	 * @param string $string
-	 * @return string
-	 */
-	protected function get_extension($string)
-	{
-		$parts = explode('.', $string);
-		return array_pop($parts);
-	}
-	
-	/**
-	 * Displays the file
-	 *
-	 * @param string $file		name of the file
-	 * @param string $ext		extenion to search for
-	 */
-	protected function display_file( $file, $ext ){
-		// set header				
-		$this->request->headers['Content-Type'] = Kohana::config("mimes.$ext");
-				
-		$source = Kohana::find_file('views', $file, $ext );
-		
-		if($source AND ! in_array($ext, $this->_images))
+		// Get the file path from the request
+		$file = $this->request->param('file');
+
+		// Find the file extension
+		$ext = pathinfo($file, PATHINFO_EXTENSION);
+
+		// Remove the extension from the filename
+		$path = substr($file, 0, -(strlen($ext) + 1));
+
+		// Find the full path to the file
+		$path = Kohana::find_file('media', $path, $ext);
+
+		if ( ! $path)
 		{
-			include $source;
+			throw new Shindig_Exception('File does not exist: :file', array(':file' => $file));
 		}
-		
-		if($source AND in_array($ext, $this->_images))
-		{
-			$this->request->headers['Content-length'] = filesize($source);
-			$file = @ fopen($source, 'rb');
-			if ($file) { 
-				fpassthru($file); 
-				exit; 
-			} 
-		} 
-	}		
+
+		// Set the content type for this extension
+		$this->request->headers['Content-Type']   = File::mime_by_ext($ext);
+		$this->request->headers['Content-Length'] = filesize($path);
+
+		// Open the file and send the entire contents
+		$file = fopen($path, 'rb');
+		fpassthru($file);
+		fclose($file);
+	}
+
 }
