315d33a173e8ddff44427ea1cbc5ce1e92bcce58
Woody Gilk <woody@wingsc.com>
2009-12-02 10:52:20 -0600

    Updated "tag" model, removed unnecessary field attributes, made slugs unique

--- classes/model/shindig/tag.php
+++ classes/model/shindig/tag.php
@@ -8,15 +8,14 @@ class Model_Shindig_Tag extends Sprig
 	protected function _init()
 	{
 		$this->_fields += array(
-			'id' => new Sprig_Field_Auto(array(
-				'primary'=>TRUE,
-			)),
+			'id' => new Sprig_Field_Auto,
 			'tag' => new Sprig_Field_Char(array(
 				'label' => __('Tag'),
 			)),
 			'slug' => new Sprig_Field_Char(array(
-				'label' => __('Slug'),
-				'edit' => FALSE,
+				'label'    => __('Slug'),
+				'editable' => FALSE,
+				'unique'   => TRUE,
 			)),
 			'posts' => new Sprig_Field_ManyToMany(array(
 				'label' => __('Posts'),
